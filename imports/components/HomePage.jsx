04b218bdf34dbb0de4008cf83e3bcb0c7f7a1080
rigconfig <contact@rigconfig.com>
2016-10-30 03:57:27 -0700

    Edits HomePage message.

--- imports/components/HomePage.jsx
+++ imports/components/HomePage.jsx
@@ -4,5 +4,6 @@ import React from 'react';
 export const HomePage = ({ children }) =>
   <div className="HomePage">
     <h2>Home Page</h2>
+    <p>(Open browser console for messages)</p>
     { children }
   </div>
